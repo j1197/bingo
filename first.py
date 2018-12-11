4ec6e0a74dd59cfec349aa92b954092b4fdce543
J <jehlum11@gmail.com>
2018-12-11 22:21:25 +0530

    Simple functions

---
+++ first.py
@@ -0,0 +1,34 @@
+print("Welcome")
+
+def add(x, y):
+    z = x + y
+    print(z)
+
+
+def mul(x, y):
+    z = x * y
+    print(z)
+
+
+def sub(x,y):
+
+    if ( x >> y):
+        z = x - y
+        print (z)
+
+    else:
+        print (" Please give appropriate values.")
+
+
+def div(x,y):
+    if(y==0):
+        print("A division by zero is not possible, please give appropriate values.")
+
+    else:
+        z=x/y
+        print (z)
+
+
+
+
+
