6532dd7666235db6366d73cd958359d6c8bb6447
siddhantb <siddhantb@dosepack.com>
2018-12-13 07:04:58 +0530

    Do initial review, add a bunch of TODOs to improve the code.

--- first.py
+++ first.py
@@ -5,7 +5,7 @@ def add(a, b):
     z = a + b
     print(z)
 
-
+# TODO: I guess there is some serious issue with this function
 def mul(a, b):
     z = x * y
     print(z)
@@ -16,13 +16,15 @@ def sub(a, b):
         z = a - b
         print(z)
 
-    else:
-        print(" Please give appropriate values.")
+    else: # TODO: Why you're not giving negative output?
+        print(" Please give appropriate values.")  # TODO: Extra space at the beginning of sentence.
 
 
 def div(a, b):
     if b == 0:
         print("A division by zero is not possible, please give appropriate values.")
+        # TODO: What do you think about this?
+        # print("[INFO] A division by zero is not possible, please give appropriate values.")
 
     else:
         z = a / b
