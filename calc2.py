497de206840fa44f8f4b583aa18f15901daf0e87
J <jehlum11@gmail.com>
2018-12-15 11:30:58 +0530

    Continual running stopped. Error corrected.

--- calc2.py
+++ calc2.py
@@ -1,32 +1,37 @@
 import first
 
 
+def go_away():
+    print("Thank you for using the calculator! You are amazing!")
+    exit()
+
+
 def calculating():
     x = input("Please enter the first number of your choice: ")
     if x == 'q':
-        print("Thank you for using the calculator! You are amazing!")
+        go_away()
     else:
         x = int(x)
-    y = input("Please enter the second number of your choice: ")
-    if y == 'q':
-        print("Thank you for using the calculator! You are amazing!")
-    else:
-        y = int(y)
-    print("I can perform addition, subtraction, division and multiplication. What do you intend to perform?")
-    s = input("Please type the initials for each operation.")
-    s = s.lower()
-    if s == 'a':
-        first.add(x, y)
-    elif s == 's':
-        first.sub(x, y)
-    elif s == 'm':
-        first.mul(x, y)
-    elif s == 'd':
-        first.div(x, y)
-    elif s == 'q':
-        print("Thank you for using the calculator! You are amazing!")
-    else:
-        print("This is beyond my scope of learning as of now. Please try again.")
+        y = input("Please enter the second number of your choice: ")
+        if y == 'q':
+            go_away()
+        else:
+            y = int(y)
+            print("I can perform addition, subtraction, division and multiplication. What do you intend to perform?")
+            s = input("Please type the initials for each operation.")
+            s = s.lower()
+            if s == 'a':
+                first.add(x, y)
+            elif s == 's':
+                first.sub(x, y)
+            elif s == 'm':
+                first.mul(x, y)
+            elif s == 'd':
+                first.div(x, y)
+            elif s == 'q':
+                go_away()
+            else:
+                print("This is beyond my scope of learning as of now. Please try again.")
 
 
 while True:
@@ -35,5 +40,7 @@ while True:
     else:
         calculating()
 
-# TODO: Baby doll you're not using method go correctly rethink about it
+
+
+
 
